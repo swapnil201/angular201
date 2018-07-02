2953b2055f6f05ac008b4784dde004ce6386b95c
Swapnil Nayak <M1041656@mindtree.com>
2018-07-02 21:09:00 +0530

    first component

--- Practice/my-first-app/src/app/app.module.ts
+++ Practice/my-first-app/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
+import { SeverComponent } from './server/server.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SeverComponent
   ],
   imports: [
     BrowserModule
